c79e37537e4f629c5e934f1b751cc6a9e054a95b
guohw <guohw@codoon.com>
2016-08-20 18:20:16 +0800

    增加redis配置

---
+++ www/common/sys_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def get_sys_config(key):
+    config = DBConfig.objects.filter(key=key)[0]
+    if config:
+        return config.value
+    
+    return None
+
+def set_sys_config(key, value):
+    config = DBConfig.objects.filter(key=key)
+    if config:
+        config.value = value
+    else:
+        config = DBConfig(key=key, value=value)
+        config.save()
+
--- www/wx_base/backends/common.py
+++ www/wx_base/backends/common.py
@@ -1,40 +1,46 @@
 #coding=utf8
-'''
-Created on 2014-5-19
-通用帮助函数
-
-@author: skycrab
-'''
 import hmac
 import json
 
+from settings import REDIS_WEIXIN
 from .. import WeixinHelper, class_property
-
 
 class CommonHelper(object):
 
-    @class_property
-    def expire(cls):
-        """比真实过期减少时间"""
-        return 300
+    redis_clt = None
+
+    def get_redis():
+        if not redis_clt:
+            redis_clt = redis.StrictRedis(host=REDIS_WEIXIN[0], port=REDIS_WEIXIN[1], db=REDIS_WEIXIN[2])
+            return redis_clt
+
+        # old connection, check it  缺乏redis异常处理流程
+        # if not redis_clt.ping():
+        #    redis_clt = redis.StrictRedis(host='localhost', port=6379, db=0) 
+
+        return redis_clt
+
 
     @class_property
-    def cache(cls):
-        """返回cache对象"""
+    def expire(cls):
+        """比真实过期减少时间2分钟"""
+        return 120
 
 
     @class_property
     def access_token_key(cls):
-        return "ACCESS_TOKEN"
+        return "WEIXIN_ACCESS_TOKEN"
+
 
     @class_property
     def jsapi_ticket_key(cls):
-        return "JSAPI_TICKET"
+        return "WEIXIN_JSAPI_TICKET"
 
 
     @class_property
     def access_token(cls):
-        cache, key = cls.cache, cls.access_token_key
+        cache = get_redis()
+        key = cls.access_token_key
         token = cache.get(key)
         if not token:
             data = json.loads(WeixinHelper.getAccessToken())
@@ -45,7 +51,8 @@ class CommonHelper(object):
 
     @class_property
     def jsapi_ticket(cls):
-        cache, key = cls.cache, cls.jsapi_ticket_key
+        cache = get_redis()
+        key = cls.jsapi_ticket_key
         ticket = cache.get(key)
         if not ticket:
             data = json.loads(WeixinHelper.getJsapiTicket(cls.access_token))
@@ -53,10 +60,12 @@ class CommonHelper(object):
             cache.set(key, ticket, expire-cls.expire)
         return ticket
 
+
     @classmethod
     def send_text_message(cls, openid, message):
         """客服主动推送消息"""
         return WeixinHelper.sendTextMessage(openid, message, cls.access_token)
+
 
     @classmethod
     def jsapi_sign(cls, url):
@@ -68,10 +77,12 @@ class CommonHelper(object):
     def hmac_sign(cls, key):
         return hmac.new(cls.secret_key, key).hexdigest()
 
+
     @classmethod
     def sign_cookie(cls, key):
         """cookie签名"""
         return "{0}|{1}".format(key, cls.hmac_sign(key))
+
 
     @classmethod
     def check_cookie(cls, value):
